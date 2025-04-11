e7f9badd7ed01c001447bc0cf59e33f6ebcd9859
John Koster <john@stillat.com>
2025-04-11 10:26:12 -0500

    Refactors
    Now that all of the component infrastructure is inside the Flux compiler, we can start to inject our hooks and modify things to support the cache features

--- src/Compiler/TagCompiler.php
+++ src/Compiler/TagCompiler.php
@@ -3,7 +3,10 @@
 namespace Flux\Compiler;
 
 use Illuminate\Support\Str;
+use Illuminate\View\AnonymousComponent;
+use Illuminate\View\Compilers\BladeCompiler;
 use Illuminate\View\Compilers\ComponentTagCompiler;
+use Illuminate\View\DynamicComponent;
 use Stillat\BladeParser\Nodes\Components\ComponentNode;
 use Stillat\BladeParser\Parser\DocumentParser;
 
@@ -108,16 +111,66 @@ class TagCompiler extends ComponentTagCompiler
         return $this->compileFluxTag($node);
     }
 
+    protected function compileFluxComponent($expression)
+    {
+        [$component, $alias, $data] = str_contains($expression, ',')
+            ? array_map(trim(...), explode(',', trim($expression, '()'), 3)) + ['', '', '']
+            : [trim($expression, '()'), '', ''];
+
+        $component = trim($component, '\'"');
+
+        $hash = ComponentHashes::newComponentHash(
+            $component === AnonymousComponent::class ? $component.':'.trim($alias, '\'"') : $component
+        );
+
+        if (Str::contains($component, ['::class', '\\'])) {
+            return BladeCompiler::compileClassComponentOpening($component, $alias, $data, $hash);
+        }
+
+        return "<?php \$__env->startComponent{$expression}; ?>";
+    }
+
     private function fluxComponentString(ComponentNode $node, $attributes = null)
     {
         if ($attributes === null) {
             $attributes = $this->getAttributesFromAttributeString($node->parameterContent);
         }
 
-        return $this->componentString(
-            'flux::'.$node->name,
-            $attributes
-        );
+        $component = 'flux::'.$node->name;
+
+        $class = $this->componentClass($component);
+
+        [$data, $attributes] = $this->partitionDataAndAttributes($class, $attributes);
+
+        $data = $data->mapWithKeys(function ($value, $key) {
+            return [Str::camel($key) => $value];
+        });
+
+        // If the component doesn't exist as a class, we'll assume it's a class-less
+        // component and pass the component as a view parameter to the data so it
+        // can be accessed within the component and we can render out the view.
+        if (! class_exists($class)) {
+            $view = Str::startsWith($component, 'mail::')
+                ? "\$__env->getContainer()->make(Illuminate\\View\\Factory::class)->make('{$component}')"
+                : "'$class'";
+
+            $parameters = [
+                'view' => $view,
+                'data' => '['.$this->attributesToString($data->all(), $escapeBound = false).']',
+            ];
+
+            $class = AnonymousComponent::class;
+        } else {
+            $parameters = $data->all();
+        }
+
+        $compiledComponent = $this->compileFluxComponent("'{$class}', '{$component}', [".$this->attributesToString($parameters, $escapeBound = false).']');
+
+        return "##BEGIN-COMPONENT-CLASS##{$compiledComponent}".'
+<?php if (isset($attributes) && $attributes instanceof Illuminate\View\ComponentAttributeBag): ?>
+<?php $attributes = $attributes->except(\\'.$class.'::ignoredParameterNames()); ?>
+<?php endif; ?>
+<?php $component->withAttributes(['.$this->attributesToString($attributes->all(), $escapeAttributes = $class !== DynamicComponent::class).']); ?>';
     }
 
     protected function compileFluxTag(ComponentNode $node)
@@ -128,10 +181,13 @@ class TagCompiler extends ComponentTagCompiler
             $class = \Illuminate\View\AnonymousComponent::class;
 
             // Laravel 12+ uses xxh128 hashing for views https://github.com/laravel/framework/pull/52301...
-            return "<?php if (!Flux::componentExists(\$name = {$component})) throw new \Exception(\"Flux component [{\$name}] does not exist.\"); ?>##BEGIN-COMPONENT-CLASS##@component('{$class}', 'flux::' . {$component}, [
+            $expression = "'{$class}', 'flux::' . {$component}, [
     'view' => (app()->version() >= 12 ? hash('xxh128', 'flux') : md5('flux')) . '::' . {$component},
     'data' => \$__env->getCurrentComponentData(),
-])
+]";
+            $compiledComponent = $this->compileFluxComponent($expression);
+
+            return "<?php if (!Flux::componentExists(\$name = {$component})) throw new \Exception(\"Flux component [{\$name}] does not exist.\"); ?>##BEGIN-COMPONENT-CLASS##{$compiledComponent}
 <?php \$component->withAttributes(\$attributes->getAttributes()); ?>".$this->compileChildNodes($node).$this->compileClosingTag($node->isClosedBy);
         }
 
@@ -148,14 +204,36 @@ class TagCompiler extends ComponentTagCompiler
 
             unset($attributes['slot']);
 
-            return '@slot('.$slot.') ' . $this->fluxComponentString($node, $attributes)."\n@endComponentClass##END-COMPONENT-CLASS##" . ' @endslot';
+            return '@slot('.$slot.') ' . $this->fluxComponentString($node, $attributes)."\n".$this->compileEndComponentClass()."##END-COMPONENT-CLASS##" . ' @endslot';
         }
 
-        return $this->fluxComponentString($node)."\n@endComponentClass##END-COMPONENT-CLASS##";
+        return $this->fluxComponentString($node)."\n".$this->compileEndComponentClass()."##END-COMPONENT-CLASS##";
     }
 
     protected function compileClosingTag(ComponentNode $node)
     {
-        return ' @endComponentClass##END-COMPONENT-CLASS##';
+        return ' '.$this->compileEndComponentClass().'##END-COMPONENT-CLASS##';
+    }
+
+    protected function compileEndComponent()
+    {
+        return '<?php echo $__env->renderComponent(); ?>';
+    }
+
+    protected function compileEndComponentClass()
+    {
+        $hash = ComponentHashes::popHash();
+
+        return $this->compileEndComponent()."\n".implode("\n", [
+                '<?php endif; ?>',
+                '<?php if (isset($__attributesOriginal'.$hash.')): ?>',
+                '<?php $attributes = $__attributesOriginal'.$hash.'; ?>',
+                '<?php unset($__attributesOriginal'.$hash.'); ?>',
+                '<?php endif; ?>',
+                '<?php if (isset($__componentOriginal'.$hash.')): ?>',
+                '<?php $component = $__componentOriginal'.$hash.'; ?>',
+                '<?php unset($__componentOriginal'.$hash.'); ?>',
+                '<?php endif; ?>',
+            ]);
     }
 }
